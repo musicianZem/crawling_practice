4c6c5c242f3a474dc542d205fe94612a9f67be07
musicianZem <201224510@pusan.ac.kr>
2020-05-29 18:05:17 +0900

    작업중인 내용 그대로 업로드

--- README.md
+++ README.md
@@ -1,3 +1,7 @@
 # crawling_practice
 
 This respository will be used personal practice codes.
+
+## 개인 참고용
+http://www.homeplus.co.kr/app.exhibition.category.Category.ghs?comm=category.list&cid=61351
+http://www.homeplus.co.kr/app.product.GoodDetail.ghs?comm=usr.detail&good_id=146525568&cid=61351
--- parse_homeplus_shoes_by_click.py
+++ parse_homeplus_shoes_by_click.py
@@ -4,15 +4,30 @@ import getpass
 from selenium import webdriver # pip install selenium
 from bs4 import BeautifulSoup as bs # pip install bs4
 from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.support.wait import WebDriverWait
+from selenium.webdriver.common.by import By
 
 # chrome 드라이버
 driver = webdriver.Chrome('./chromedriver83.exe')
 driver.get('http://www.homeplus.co.kr/app.exhibition.category.Category.ghs?comm=category.list&cid=61351')
 
-shop_list = driver.find_elements_by_class_name('fix-ty2')
+for i in range(11) :
+    for j in range(10) :
+        if(i == 8 and j == 3) :
+            break
 
-for elem in shop_list :
-    shoes_link_list = elem.find_elements_by_css_selector('li')
+        #driver.execute_script("jsPage()") # wait.....
+        shop_list = driver.find_elements_by_class_name('fix-ty2')
 
-    for slink in shoes_link_list :
-        slink.find_element_by_css_selector('div').click()
+        for elem in shop_list :
+            shoes_link_list = elem.find_elements_by_css_selector('li')
+
+            for slink in shoes_link_list :
+                shoes_image = slink.find_element_by_css_selector('img')
+                onclick_function = shoes_image.get_attribute('onclick')
+                if(onclick_function) :
+                    print(onclick_function) ## result = goodsid
+                #slink.find_element_by_css_selector('div').click()
+                #e = WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.CLASS_NAME, "p-info")))
+                #driver.execute_script("window.history.go(-1)")
